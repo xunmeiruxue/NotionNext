3933eb6cb4bc766a9252580c1a200b07855aeb69
xunmeiruxue <93825155+xunmeiruxue@users.noreply.github.com>
2025-05-06 20:22:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '晏清辉', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '金猴奋起千钧棒，玉宇澄清万里埃', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://sufangmu.net', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://fangmu.net', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '晏清辉', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
+    '金猴奋起千钧棒，玉宇澄清万里埃',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
